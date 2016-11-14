d9100b1580cc5f7adfe719ca6bac650fd314d47c
glikeria <sl.ellen.al@gmail.com>
2016-11-14 23:25:39 +0200

    committing first modification

--- task_dict_symbols.py
+++ task_dict_symbols.py
@@ -1,3 +1,6 @@
+#I just needed to add something! Vivat modification!!!
+print "**********"
+print "this is test task for Git"
 symbols = {}
 string = 'Additional Resource Links Visit https://www.python.org/downloads/ to download the latest version of Python 2.x See Introduction to Python for information on how to introduce and use Python in your curriculum See the Python Basics Quick Reference for an overview of the basic features of Python Administrative Details Contact info For more info about Exploring Computational Thinking (ECT), visit the ECT website (g.co/exploringCT) Credits Developed by the Exploring Computational Thinking team at Google and reviewed by K-12 educators from around the world. Last updated on 01/15/2015 Copyright info Except as otherwise noted, the content of this document is licensed under the Creative Commons Attribution 4.0 International License, and code samples are licensed under the Apache 2.0 License.'
 for symbol in string.lower():
